bb7e0ddf1c5b7bd9579a222989e5f44031f3bc01
이성규 <cheers8oys@gmail.com>
2025-05-20 15:06:44 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 52.37 ms, Memory: 91.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        String[] num = number.split("");
+        int sum = 0;
+        for(int i = 0; i < num.length; i++){
+            sum += Integer.parseInt(num[i]);
+        }
+        answer = sum % 9;
+        return answer;
+    }
+}
